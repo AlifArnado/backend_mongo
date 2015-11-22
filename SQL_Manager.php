5d402ab28b8f37df7e8d8980ccce3380182a9a28
AlifArnado <benden.alif@gmail.com>
2015-11-22 21:05:35 +0700

    first commit

--- SQL_Manager.php
+++ SQL_Manager.php
@@ -4,28 +4,39 @@
         public $database;
         public $collection;
 
-        public function get_Koneksi($database_name)
+        /*----------  setting database MongoDB  ----------*/
+        public function get_KoneksiMongoDB()
            {
                try {
+                    // bila berhasil
                    $this->koneksi = new Mongo();
-                   $this->database = $this->koneksi->selectDB($database_name);
-                   echo "berhasil";
                } catch (Exception $e) {
+                    // bila problem
                    die($e->getMessage());
                }
            }
+
+        // setting nama databse
+        public function set_MongoDatabase($database_name) {
+            if(($this->database_name = $database_name) == NULL) {
+                echo "Parameter database NotFound";
+            } else {
+                return $this->database = $this->koneksi->selectDB($database_name);
+            }
+        }
+
         public function get_Collection($collection_name) {
             return $this->collection = $this->database->selectCollection($collection_name);
         }
-
     }
 
     /*----------  testing  ----------*/
     $sql = new SQL_Manager();
-    $sql->get_Koneksi("backend_mongodb");
+    $sql->get_KoneksiMongoDB();
+    $sql->set_MongoDatabase("backend_mongodb");
     $collection_create = $sql->get_Collection("collection_mongodb");
 
-    $data = array('nama' => "Alif Benden Arnado", "nim" => 125410148);
+    $data = array('nama' => "Alif Benden", "nim" => 12541);
     $repost = $collection_create->insert($data);
     if(!$repost) {
         die("data not insert");
